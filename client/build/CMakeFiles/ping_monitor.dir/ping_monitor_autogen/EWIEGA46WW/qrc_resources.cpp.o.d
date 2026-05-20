f8a85309ecda3976509192f0b53d9fca1a16bb05
el0qu4nce <el0qu4nce@gmail.com>
2026-05-20 16:30:07 +0300

    build termination

--- client/build/CMakeFiles/ping_monitor.dir/ping_monitor_autogen/EWIEGA46WW/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/ping_monitor.dir/ping_monitor_autogen/EWIEGA46WW/qrc_resources.cpp.o: \
- /home/maver1ckpy/coding/projects/PRIYOMysh/client/build/ping_monitor_autogen/EWIEGA46WW/qrc_resources.cpp \
- /usr/include/stdc-predef.h
